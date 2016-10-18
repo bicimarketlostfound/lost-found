b5e5871a0df66a11427b869c883352952c570dd7
su6zero <israelalexei@gmail.com>
2016-10-18 17:28:09 +0200

    First Commit
    Primer Commit que contiene la página que tenemos en localhost y las
    imágenes que emplearemos, además de la base de datos vacía.

---
+++ bd_projecte1_Esther_Israel.sql
@@ -0,0 +1,67 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 17-10-2016 a las 17:01:07
+-- Versión del servidor: 10.1.16-MariaDB
+-- Versión de PHP: 5.6.24
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `bd_projecte1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `anunci`
+--
+
+CREATE TABLE `anunci` (
+  `anu_id` int(4) NOT NULL,
+  `anu_titol` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_data_anunci` date NOT NULL,
+  `anu_data_robatori` date NOT NULL,
+  `anu_ubicacio_robatori` varchar(35) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_descripcio_robatori` varchar(200) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_marca` varchar(15) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_model` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_color` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_antiguitat` varchar(25) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `anu_descripcio` text COLLATE utf8_spanish_ci NOT NULL,
+  `anu_numero_serie` varchar(15) COLLATE utf8_spanish_ci DEFAULT NULL,
+  `anu_foto` varchar(25) COLLATE utf8_spanish_ci NOT NULL,
+  `anu_compensacio` decimal(4,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `anunci`
+--
+ALTER TABLE `anunci`
+  ADD PRIMARY KEY (`anu_id`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `anunci`
+--
+ALTER TABLE `anunci`
+  MODIFY `anu_id` int(4) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
